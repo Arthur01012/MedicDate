430c491aad33bd5562ce720b1e80ae50e7e48a93
Arthur Reyes <142542790+Arthur01012@users.noreply.github.com>
2026-07-14 21:49:28 -0600

    agregar conexion

---
+++ MedicDate/Procesos/clsConexion.cs
@@ -0,0 +1,44 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicDate.Procesos
+{
+    internal class clsConexion
+    {
+        private readonly string cadenaConexion = ObtenerCadenaConexion();
+
+        private static string ObtenerCadenaConexion()
+        {
+            string? fromConfig = System.Configuration.ConfigurationManager.ConnectionStrings["MedicDateDB"]?.ConnectionString;
+            if (!string.IsNullOrWhiteSpace(fromConfig))
+            {
+                return fromConfig;
+            }
+            return "";
+        }
+
+        public static MySqlConnection ObtenerConexio()
+        {
+            try
+            {
+                MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+                if (conexion.State == ConnectionState.Closed)
+                {
+                    conexion.Open();
+                }
+                return conexion;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al conectar con la base de datos: " + ex.Message);
+            }
+        }
+
+    }
+}
