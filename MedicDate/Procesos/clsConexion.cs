17505aa28eedf4b860923acfe4abf674e8fced8f
Arthur Reyes <142542790+Arthur01012@users.noreply.github.com>
2026-07-14 22:54:18 -0600

    inicio de formulario login

--- MedicDate/Procesos/clsConexion.cs
+++ MedicDate/Procesos/clsConexion.cs
@@ -1,6 +1,7 @@
 ﻿using MySqlConnector;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -10,19 +11,21 @@ namespace MedicDate.Procesos
 {
     internal class clsConexion
     {
-        private readonly string cadenaConexion = ObtenerCadenaConexion();
+        private static readonly string cadenaConexion = ObtenerCadenaConexion();
 
         private static string ObtenerCadenaConexion()
         {
-            string? fromConfig = System.Configuration.ConfigurationManager.ConnectionStrings["MedicDateDB"]?.ConnectionString;
+
+            string? fromConfig = ConfigurationManager.ConnectionStrings["MedicDateDB"]?.ConnectionString;
             if (!string.IsNullOrWhiteSpace(fromConfig))
             {
                 return fromConfig;
             }
+
             return "";
         }
 
-        public static MySqlConnection ObtenerConexio()
+        public static MySqlConnection ObtenerConexion()
         {
             try
             {
@@ -32,13 +35,11 @@ namespace MedicDate.Procesos
                     conexion.Open();
                 }
                 return conexion;
-
             }
             catch (Exception ex)
             {
                 throw new Exception("Error al conectar con la base de datos: " + ex.Message);
             }
         }
-
     }
 }
