78646cd06910aa451acb51044267067d162ef75d
Arthur Reyes <142542790+Arthur01012@users.noreply.github.com>
2026-07-30 16:08:29 -0600

    sin completar -  falta paginacion

---
+++ MedicDate/Procesos/clsPacienteDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+namespace MedicDate.Procesos
+{
+    internal class clsPacienteDAL
+    {
+        public DataTable cargarDataGrid()
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                using var conexion = clsConexion.ObtenerConexion();
+                string sql = @"SELECT 
+                
+                ";
+                using var adapter =  new MySqlDataAdapter(sql, conexion);
+                adapter.Fill(tabla);
+                return tabla;
+            }
+            catch(Exception ex) {
+            
+                throw new Exception("Error al cargar los datos del paciente: " + ex.Message); 
+            }
+
+        }
+    }
+}
