ff0a3b0f2877b3b631b9e32f5868cfac1ada09ec
Arthur Reyes <142542790+Arthur01012@users.noreply.github.com>
2026-07-31 14:59:37 -0600

    agenda general funciona

--- MedicDate/CapaPresentacion/frmAgendaGeneral.cs
+++ MedicDate/CapaPresentacion/frmAgendaGeneral.cs
@@ -12,41 +12,39 @@ namespace MedicDate.CapaPresentacion
             InitializeComponent();
         }
 
+        
         private void frmAgendaGeneral_Load(object sender, EventArgs e)
         {
-            CargarAgenda();
+            dtpFecha.Value = DateTime.Today; // Establecemos la fecha de hoy
+            ActualizarAgenda();              // Cargamos la agenda del día de hoy
         }
 
-        private void CargarAgenda()
+        
+        private void ActualizarAgenda()
         {
             try
             {
-                if (string.IsNullOrWhiteSpace(txtBuscarDoctor.Text))
-                {
-                    dgvCitas.DataSource = clsCitaDAL.CargarDataGrid(dtpFecha.Value);
-                }
-                else
-                {
-                    dgvCitas.DataSource = clsCitaDAL.Consultar(dtpFecha.Value, txtBuscarDoctor.Text);
-                }
+                DataTable tabla = clsCitaDAL.Consultar(dtpFecha.Value, txtBuscarDoctor.Text);
 
+                dgvCitas.DataSource = tabla;
                 dgvCitas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dgvCitas.Columns["id_cita"].Visible = false; // Oculta el ID de la cita
+                dgvCitas.Columns["Fecha"].DefaultCellStyle.Format = "dd/MM/yyyy"; // Formato de fecha
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error al cargar agenda:\n\n{ex.Message}", "Error Crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Error al cargar la agenda: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void dtpFecha_ValueChanged(object sender, EventArgs e)
         {
-            CargarAgenda();
+            ActualizarAgenda();
         }
-
-
+       
         private void txtBuscarDoctor_TextChanged(object sender, EventArgs e)
         {
-            CargarAgenda(); 
+            ActualizarAgenda();
         }
     }
 }
