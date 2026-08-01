02e7606a7d59f73c0ca372201e9ad0bda268bd5a
monserrat castro hernandez <monserratcastrohernandez30@gmail.com>
2026-08-01 09:50:37 -0600

    guardado de pacientes

--- MedicDate/CapaPresentacion/frmPaciente.cs
+++ MedicDate/CapaPresentacion/frmPaciente.cs
@@ -7,19 +7,322 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MedicDate.Datos;
+using MedicDate.Helpers;
+using MedicDate.Procesos;
 
 namespace MedicDate.CapaPresentacion
 {
     public partial class frmPaciente : Form
     {
+        // Variables
+        private clsPaciente paciente = new clsPaciente();
+        private int? idPacienteEditar = null;
+
+        // Constructor
         public frmPaciente()
         {
             InitializeComponent();
+            // Asegura que los manejadores estén registrados
+
+            ConfigurarFormulario();
+            CargarMunicipios();
         }
 
-        private void lblNotas_Click(object sender, EventArgs e)
+        private void CargarMunicipios()
         {
+            try
+            {
+                DataTable municipios = clsMunicipioDAL.ObtenerMunicipios();
+
+                cmbMunicipio.DataSource = municipios;
+                cmbMunicipio.DisplayMember = "nombre";
+                cmbMunicipio.ValueMember = "id_municipio";
+                cmbMunicipio.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar municipios: " + ex.Message);
+            }
+        }
+
+
+        // Configuración inicial del formulario
+        private void ConfigurarFormulario()
+        {
+            // No permite seleccionar una fecha mayor a la actual
+            dtpFechaRegistro.MaxDate = DateTime.Today;
+
+            // El ComboBox de municipios inicia sin ninguna selección
+            cmbMunicipio.SelectedIndex = -1;
+
+            // Coloca el cursor en el primer campo
+            tctNombreP.Focus();
+        }
+
+        // Validar datos ingresados
+        private bool ValidarDatos()
+        {
+            // Nombre
+            if (string.IsNullOrEmpty(tctNombreP.Text))
+            {
+                MessageBox.Show("El nombre es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                tctNombreP.Focus();
+                return false;
+            }
+
+            //Apellido Paterno
+            if (string.IsNullOrEmpty(txtAPaterno.Text))
+            {
+                MessageBox.Show("El apellido paterno es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtAPaterno.Focus();
+                return false;
+            }
+
+            //Apellido Materno
+            if (string.IsNullOrEmpty(txtAMaterno.Text))
+            {
+                MessageBox.Show("El apellido materno es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtAMaterno.Focus();
+                return false;
+            }
+            //Gmail
+            if (string.IsNullOrEmpty(txtEmal.Text))
+            {
+                MessageBox.Show("El email es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtEmal.Focus();
+                return false;
+            }
+
+            // Verifica que el formato del correo sea válido
+            if (!clsValidaciones.EsEmailValido(txtEmal.Text))
+            {
+                MessageBox.Show("El email no es válido.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtEmal.Focus();
+                return false;
+            }
+
+            // Teléfono 
+            if (string.IsNullOrEmpty(txtTelefono.Text))
+            {
+                MessageBox.Show("El teléfono es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtTelefono.Focus();
+                return false;
+            }
+
+
+            // Verifica que el número de teléfono tenga un formato válido
+            if (!clsValidaciones.EsTelefonoValido(txtTelefono.Text))
+            {
+                MessageBox.Show("El teléfono no es válido.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtTelefono.Focus();
+                return false;
+            }
+
+            //telefono secundario
+            if (string.IsNullOrEmpty(txtTelefonoSec.Text))
+            {
+                MessageBox.Show("El teléfono secundario es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtTelefonoSec.Focus();
+                return false;
+            }
+
+            // Verifica que el número de teléfono secundario
+            if (!clsValidaciones.EsTelefonoValido(txtTelefonoSec.Text))
+            {
+                MessageBox.Show("El teléfono secundario no es válido.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtTelefonoSec.Focus();
+                return false;
+            }
+
+            //Fecha de nacimiento 
+            if (dtpFechaRegistro.Value > DateTime.Today)
+            {
+                MessageBox.Show("La fecha de nacimiento no puede ser futura.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                dtpFechaRegistro.Focus();
+                return false;
+            }
+
+            //Calle 
+            if (string.IsNullOrEmpty(txtCalle.Text))
+            {
+                MessageBox.Show("La calle es obligatoria.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtCalle.Focus();
+                return false;
+            }
+
+            //Colonia 
+            if (string.IsNullOrEmpty(txtColonia.Text))
+            {
+                MessageBox.Show("La colonia es obligatoria.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtColonia.Focus();
+                return false;
+            }
+
+            //Numero
+            if (string.IsNullOrEmpty(txtNumero.Text))
+            {
+                MessageBox.Show("El número es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtNumero.Focus();
+                return false;
+            }
+            //Localidadd
+            if (string.IsNullOrEmpty(txtLocalidad.Text))
+            {
+                MessageBox.Show("La localidad es obligatoria.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtLocalidad.Focus();
+                return false;
+            }
+            //Municipio
+            if (cmbMunicipio.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione un municipio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                cmbMunicipio.Focus();
+                return false;
+            }
+
+            //Alergias
+            if (string.IsNullOrEmpty(txtAlergias.Text))
+            {
+                MessageBox.Show("El campo Alergias es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtAlergias.Focus();
+                return false;
+            }
+
+            //Nota
+            if (string.IsNullOrEmpty(txtNotas.Text))
+            {
+                MessageBox.Show("El campo Notas es obligatorio.", "Validación",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                txtNotas.Focus();
+                return false;
+            }
+
+            return true;
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Si alguna validación falla, no continúa
+            if (!ValidarDatos()) return;
+
+            using var conexion = clsConexion.ObtenerConexion();
+            using var transaccion = conexion.BeginTransaction();
+
+            try
+            {
+                // Datos del paciente
+                paciente.nombre = tctNombreP.Text.Trim();
+                paciente.apellido_paterno = txtAPaterno.Text.Trim();
+                paciente.apellido_materno = txtAMaterno.Text.Trim();
+                paciente.email = txtEmal.Text.Trim();
+                paciente.telefono_principal = txtTelefono.Text.Trim();
+                paciente.telefono_secundario = txtTelefonoSec.Text.Trim();
+                paciente.fecha_nacimiento = dtpFechaRegistro.Value;
+                // Dirección
+                paciente.calle = txtCalle.Text.Trim();
+                paciente.colonia = txtColonia.Text.Trim();
+                paciente.numero = txtNumero.Text.Trim();
+                paciente.localidad = txtLocalidad.Text.Trim();
+                paciente.id_municipio = (int?)cmbMunicipio.SelectedValue;
+                // Información médica
+                paciente.alergias = txtAlergias.Text.Trim();
+                paciente.notas_medicas = txtNotas.Text.Trim();
+
+                // Guardar el paciente en la base de datos.
+                int idPaciente = clsPacienteDAL.Insertar(paciente, transaccion);
+
+                if (idPaciente > 0)
+                {
+                    transaccion.Commit();
+
+                    MessageBox.Show("Paciente registrado correctamente.",
+                        "Información",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                    LimpiarFormulario();
+                }
+                else
+                {
+                    transaccion.Rollback();
+
+                    MessageBox.Show("No se pudo registrar el paciente.",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception ex)//si ocurre un error se cansela la transaccion y se muestra un mensaje de error
+            {
+                transaccion.Rollback();
+
+                MessageBox.Show($"Error: {ex.Message}",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
 
         }
+
+        private void LimpiarFormulario()
+        {
+            tctNombreP.Clear();
+            txtAPaterno.Clear();
+            txtAMaterno.Clear();
+            txtEmal.Clear();
+            txtTelefono.Clear();
+            dtpFechaRegistro.Value = DateTime.Today;
+            txtCalle.Clear();
+            txtColonia.Clear();
+            txtNumero.Clear();
+            txtLocalidad.Clear();
+            cmbMunicipio.SelectedIndex = -1;
+            txtAlergias.Clear();
+            txtNotas.Clear();
+
+            // Regresa el cursor al primer campo
+
+            tctNombreP.Focus();
+        }
+        private void btnCancelar1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }
+
